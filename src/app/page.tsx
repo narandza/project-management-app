7126cebd6e82f66a38b17fc8eda5dfa0cd803ad9
narandza <60319154+narandza@users.noreply.github.com>
2024-10-18 23:24:31 +0200

    customize shadcn components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,12 @@ export default function Home() {
   return (
     <div className="">
       <Button> Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="muted">Muted</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="tertiary">Tertiary</Button>
     </div>
   );
 }
