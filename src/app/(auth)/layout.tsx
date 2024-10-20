57c928cbe2718499630f0d0cd4a3163d223f4c33
narandza <60319154+narandza@users.noreply.github.com>
2024-10-20 10:26:13 +0200

    create auth navbar; change default font; change project titile;

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 interface AuthLayoutProps {
@@ -6,10 +7,18 @@ interface AuthLayoutProps {
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
-    <div className="">
-      <Image src="logo.svg" alt="Logo" width={50} height={50} />
-      {children}
-    </div>
+    <main className="bg-neutral-100 min-h-screen">
+      <div className="mx-auto max-w-screen-2xl p-4">
+        <nav className="flex justify-between items-center">
+          <Image src="/logo.svg" alt="logo" width={152} height={56} />
+
+          <Button variant="secondary">Sign Up</Button>
+        </nav>
+        <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
+          {children}
+        </div>
+      </div>
+    </main>
   );
 };
 
