f04b8f340310e107eac48b986c2d1f0ad9718092
narandza <60319154+narandza@users.noreply.github.com>
2024-10-21 13:39:42 +0200

    create inital signInCard; create dotted separator component;

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,5 +1,9 @@
+"use client";
+
+import { SignInCard } from "@/features/auth/components/sign-in-card";
+
 const SignInPage = () => {
-  return <div className="">Hello</div>;
+  return <SignInCard />;
 };
 
 export default SignInPage;
