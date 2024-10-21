f7a0a3d11a41708afe07a66bf419e0c20c17ebcd
narandza <60319154+narandza@users.noreply.github.com>
2024-10-21 13:46:17 +0200

    intall react-icons; add alt login methods in sing in card;

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -3,6 +3,9 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+
 export const SignInCard = () => {
   return (
     <Card className="w-full h-full md:w-[487px] border-none shadow-none">
@@ -36,6 +39,29 @@ export const SignInCard = () => {
             </Button>
           </form>
         </CardContent>
+        <div className="px-7 ">
+          <DottedSeparator />
+          <CardContent className="p-7 flex flex-col gap-y-7">
+            <Button
+              variant="secondary"
+              size="lg"
+              className="w-full"
+              disabled={false}
+            >
+              <FcGoogle className="mr-2 size-5" />
+              Login with Google
+            </Button>
+            <Button
+              variant="secondary"
+              size="lg"
+              className="w-full"
+              disabled={false}
+            >
+              <FaGithub className="mr-2 size-5" />
+              Login with Github
+            </Button>
+          </CardContent>
+        </div>
       </div>
     </Card>
   );
