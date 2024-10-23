ead2c24cf38383e86910a7ca615fbce455d5ec45
narandza <60319154+narandza@users.noreply.github.com>
2024-10-23 11:37:59 +0200

    implenet login and logout sessions

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -5,23 +5,14 @@ import { loginSchema, registerSchema } from "../schemas";
 import { createAdminClient } from "@/lib/appwrtie";
 import { ID } from "node-appwrite";
 
-import { setCookie } from "hono/cookie";
+import { deleteCookie, setCookie } from "hono/cookie";
 import { AUTH_COOKIE } from "../constants";
 
 const app = new Hono()
   .post("/login", zValidator("json", loginSchema), async (c) => {
     const { email, password } = c.req.valid("json");
 
-    console.log({ email, password });
-
-    return c.json({ email, password });
-  })
-  .post("/register", zValidator("json", registerSchema), async (c) => {
-    const { name, email, password } = c.req.valid("json");
-
     const { account } = await createAdminClient();
-
-    const user = await account.create(ID.unique(), email, password, name);
 
     const session = await account.createEmailPasswordSession(email, password);
 
@@ -33,7 +24,31 @@ const app = new Hono()
       maxAge: 60 * 60 * 24 * 30,
     });
 
-    return c.json({ data: user });
+    return c.json({ success: true });
+  })
+  .post("/register", zValidator("json", registerSchema), async (c) => {
+    const { name, email, password } = c.req.valid("json");
+
+    const { account } = await createAdminClient();
+
+    await account.create(ID.unique(), email, password, name);
+
+    const session = await account.createEmailPasswordSession(email, password);
+
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 60 * 60 * 24 * 30,
+    });
+
+    return c.json({ success: true });
+  })
+  .post("/logout", (c) => {
+    deleteCookie(c, AUTH_COOKIE);
+
+    return c.json({ success: true });
   });
 
 export default app;
