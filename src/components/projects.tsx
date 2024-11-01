123c1f47be1a5a5e1ee2092be6a258c79b2db659
narandza <60319154+narandza@users.noreply.github.com>
2024-11-01 10:00:25 +0100

    addon projects component

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,8 +1,18 @@
 "use client";
 
+import { useGetProjects } from "@/features/projects/api/use-get-projects";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { RiAddCircleFill } from "react-icons/ri";
 
 export const Projects = () => {
+  const projectId = null; // TODO: use useProjectId
+  const pathname = usePathname();
+  const workspaceId = useWorkspaceId();
+  const { data } = useGetProjects({ workspaceId });
+
   return (
     <div className="flex flex-col gap-y-2">
       <div className="flex items-center justify-between">
@@ -12,6 +22,23 @@ export const Projects = () => {
           onClick={() => {}}
         />
       </div>
+      {data?.documents.map((project) => {
+        const href = `/workspaces/${workspaceId}/projects/${projectId}`;
+        const isActive = pathname === href;
+
+        return (
+          <Link href={href} key={project.$id}>
+            <div
+              className={cn(
+                "flex items-center gap-2.5 p-2.5 rounded-md hover:opacity-75 transition cursor-pointer text-neutral-500",
+                isActive && "bg-white shadow-sm hover:opacity-100 text-primary"
+              )}
+            >
+              <span className="truncate">{project.name}</span>
+            </div>
+          </Link>
+        );
+      })}
     </div>
   );
 };
