f2bbe70b99d0d6a39dfc31eefb5e45a6759b5df6
narandza <60319154+narandza@users.noreply.github.com>
2024-11-04 11:44:27 +0100

    create global error page

---
+++ src/app/error.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { AlertTriangle } from "lucide-react";
+import Link from "next/link";
+
+const ErrorPage = () => {
+  return (
+    <div className="h-screen gap-y-4 flex items-center flex-col justify-center">
+      <AlertTriangle className="size-6" />
+      <p className="text-sm">Something went wrong</p>
+      <Button variant="secondary" size="sm" asChild>
+        <Link href="/">Back to home</Link>
+      </Button>
+    </div>
+  );
+};
+
+export default ErrorPage;
