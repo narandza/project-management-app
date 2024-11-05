75b7a740e7b07e5b6c9866d8023221d07aa3b4b7
narandza <60319154+narandza@users.noreply.github.com>
2024-11-05 09:48:40 +0100

    create useCreateTask hook

--- src/features/tasks/components/create-task-modal.tsx
+++ src/features/tasks/components/create-task-modal.tsx
@@ -2,13 +2,14 @@
 
 import { ResponsiveModal } from "@/components/responsive-modal";
 import { useCreateTaskModal } from "../hooks/use-create-task-modal";
+import { CreateTaskFormWrapper } from "./create-task-form-wrapper";
 
 export const CreateTaskModal = () => {
-  const { isOpen, setIsOpen } = useCreateTaskModal();
+  const { isOpen, setIsOpen, close } = useCreateTaskModal();
 
   return (
     <ResponsiveModal open={isOpen} onOpenChange={setIsOpen}>
-      <div className="">task form</div>
+      <CreateTaskFormWrapper onCancel={close} />
     </ResponsiveModal>
   );
 };
