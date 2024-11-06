4fa5ef3d6d6559dea93072567439b605c44cc5e7
narandza <60319154+narandza@users.noreply.github.com>
2024-11-06 10:28:06 +0100

    implement sorting in data table

--- src/features/tasks/components/columns.tsx
+++ src/features/tasks/components/columns.tsx
@@ -2,10 +2,22 @@
 
 import { ColumnDef } from "@tanstack/react-table";
 import { Task } from "../types";
+import { ArrowUpDown } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export const columns: ColumnDef<Task>[] = [
   {
     accessorKey: "name",
-    header: "Task Name",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Task name
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
   },
 ];
