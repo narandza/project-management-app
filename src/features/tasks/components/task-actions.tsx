d293dc5f2dba6b559df27e0ea9625c61d13207aa
narandza <60319154+narandza@users.noreply.github.com>
2024-11-06 11:16:18 +0100

    create inital task actions component

---
+++ src/features/tasks/components/task-actions.tsx
@@ -0,0 +1,33 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ExternalLinkIcon } from "lucide-react";
+
+interface TaskActionsProps {
+  id: string;
+  projectId: string;
+  children: React.ReactNode;
+}
+
+export const TaskActions = ({ id, projectId, children }: TaskActionsProps) => {
+  return (
+    <div className="flex justify-end">
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-48">
+          <DropdownMenuItem
+            onClick={() => {}}
+            disabled={false}
+            className="font-medium p-[10px]"
+          >
+            <ExternalLinkIcon className="size-4 mr-2 stroke-2" />
+            Task Details
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
