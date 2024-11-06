3eb368440f9266fd1b91ecd330e6563538a8635c
narandza <60319154+narandza@users.noreply.github.com>
2024-11-06 10:43:13 +0100

    add column in data table

--- src/features/tasks/components/columns.tsx
+++ src/features/tasks/components/columns.tsx
@@ -4,6 +4,8 @@ import { ColumnDef } from "@tanstack/react-table";
 import { Task } from "../types";
 import { ArrowUpDown } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { ProjectAvatar } from "@/features/projects/components/project-avatar";
+import { MemberAvatar } from "@/features/members/components/member-avatar";
 
 export const columns: ColumnDef<Task>[] = [
   {
@@ -17,6 +19,92 @@ export const columns: ColumnDef<Task>[] = [
           Task name
           <ArrowUpDown className="ml-2 h-4 w-4" />
         </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const name = row.original.name;
+
+      return <p className="line-clamp-1">{name}</p>;
+    },
+  },
+  {
+    accessorKey: "project",
+
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Project
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const project = row.original.project;
+
+      return (
+        <div className="flex items-center gap-x-2 text-sm font-medium">
+          <ProjectAvatar
+            className="size-6 "
+            name={project.name}
+            image={project.imageUrl}
+          />
+          <p className="line-clamp-1">{project.name}</p>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "assignee",
+
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Assignee
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const assignee = row.original.assignee;
+
+      return (
+        <div className="flex items-center gap-x-2 text-sm font-medium">
+          <MemberAvatar
+            className="size-6 "
+            name={assignee.name}
+            fallbackClassName="text-xs"
+          />
+          <p className="line-clamp-1">{assignee.name}</p>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "dueDate",
+
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Due Date
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const dueDate = row.original.dueDate;
+
+      return (
+        <div className="">due date</div>
+        // TODO: <TaskDate value={dueDate} />
       );
     },
   },
