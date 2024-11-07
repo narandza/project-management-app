45d43763e0a8cc73bb018c1ff847599480ad5b94
narandza <60319154+narandza@users.noreply.github.com>
2024-11-07 11:23:47 +0100

    create inital data kanban ui

--- src/features/tasks/components/edit-task-modal.tsx
+++ src/features/tasks/components/edit-task-modal.tsx
@@ -5,7 +5,7 @@ import { useEditTaskModal } from "../hooks/use-edit-task";
 import { EditTaskFormWrapper } from "./edit-task-form-wrapper";
 
 export const EditTaskModal = () => {
-  const { taskId, setTaskId, close } = useEditTaskModal();
+  const { taskId, close } = useEditTaskModal();
 
   return (
     <ResponsiveModal open={!!taskId} onOpenChange={close}>
