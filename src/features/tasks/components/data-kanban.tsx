ce9f823ecb4332b10fccf5e2a8b11984b3de384d
narandza <60319154+narandza@users.noreply.github.com>
2024-11-08 08:54:48 +0100

    create kanban column header; implement draggable in data kanban;

--- src/features/tasks/components/data-kanban.tsx
+++ src/features/tasks/components/data-kanban.tsx
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 
 import { Task, TaskStatus } from "../types";
 
-import { DragDropContext } from "@hello-pangea/dnd";
+import { DragDropContext, Draggable, Droppable } from "@hello-pangea/dnd";
+import { KanbanColumnHeader } from "./kanban-column-header";
 
 const boards: TaskStatus[] = [
   TaskStatus.BACKLOG,
@@ -52,7 +53,38 @@ export const DataKanban = ({ data }: DataKanbanProps) => {
               className="flex-1 mx-2 bg-muted p-1.5 rounded-md min-w-[200px]"
               key={board}
             >
-              {board}
+              <KanbanColumnHeader
+                board={board}
+                taskCount={tasks[board].length}
+              />
+              <Droppable droppableId={board}>
+                {(provided) => (
+                  <div
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    className="min-h-[200px] py-1.5"
+                  >
+                    {tasks[board].map((task, index) => (
+                      <Draggable
+                        key={task.$id}
+                        draggableId={task.$id}
+                        index={index}
+                      >
+                        {(provided) => (
+                          <div
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                            ref={provided.innerRef}
+                            className=""
+                          >
+                            {task.name}
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                  </div>
+                )}
+              </Droppable>
             </div>
           );
         })}
