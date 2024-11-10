cf053d5f3114c4ff464ec55764e3d022439149d3
narandza <60319154+narandza@users.noreply.github.com>
2024-11-10 18:52:36 +0100

    create custom toolbar component

--- src/features/tasks/components/event-card.tsx
+++ src/features/tasks/components/event-card.tsx
@@ -3,6 +3,8 @@ import { TaskStatus } from "../types";
 import { cn } from "@/lib/utils";
 import { MemberAvatar } from "@/features/members/components/member-avatar";
 import { ProjectAvatar } from "@/features/projects/components/project-avatar";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
+import { useRouter } from "next/navigation";
 
 interface EventCardProps {
   id: string;
@@ -27,6 +29,16 @@ export const EventCard = ({
   project,
   status,
 }: EventCardProps) => {
+  const workspaceId = useWorkspaceId();
+
+  const router = useRouter();
+
+  const onClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+
+    router.push(`/workspaces/${workspaceId}/tasks/${id}`);
+  };
+
   return (
     <div className="px-2">
       <div
@@ -38,9 +50,9 @@ export const EventCard = ({
         <p className="">{title}</p>
         <div className="flex items-center gap-x-1">
           <MemberAvatar name={assignee?.name} />
+          <div className="size-1 rounded-full bg-neutral-300" />
+          <ProjectAvatar name={project?.name} image={project?.imageUrl} />
         </div>
-        <div className="size-1 rounded-full bg-neutral-300" />
-        <ProjectAvatar name={project?.name} image={project?.imageUrl} />
       </div>
     </div>
   );
