6e877e1a8549b8ecd8a344e6cfdc9d69808ae788
narandza <60319154+narandza@users.noreply.github.com>
2024-11-18 10:59:24 +0100

    create use get workspace info api call

--- src/app/(standalone)/workspaces/[workspaceId]/join/[inviteCode]/client.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/join/[inviteCode]/client.tsx
@@ -1,11 +1,27 @@
 "use client";
 
+import { PageError } from "@/components/page-error";
+import { PageLoader } from "@/components/page-loader";
+import { useGetWorkspaceInfo } from "@/features/workspaces/api/use-get-workspace-info";
 import { JoinWorkspaceForm } from "@/features/workspaces/components/join-workspace-form";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 
 export const WorkspaceIdJoinClient = () => {
+  const workspaceId = useWorkspaceId();
+
+  const { data, isLoading } = useGetWorkspaceInfo({ workspaceId });
+
+  if (isLoading) {
+    return <PageLoader />;
+  }
+
+  if (!data) {
+    return <PageError message="Workspace not found" />;
+  }
+
   return (
     <div className="w-full lg:max-w-xl">
-      <JoinWorkspaceForm initialValues={initialValues} />
+      <JoinWorkspaceForm initialValues={data} />
     </div>
   );
 };
