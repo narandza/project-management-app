50fda865293f9793303a563747c4153a78d6d44b
narandza <60319154+narandza@users.noreply.github.com>
2024-11-19 10:23:49 +0100

    create projects list component

--- src/app/(dashboard)/workspaces/[workspaceId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/client.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import { formatDistanceToNow } from "date-fns";
 import { CalendarIcon, PlusIcon } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
@@ -11,6 +12,7 @@ import { Card, CardContent } from "@/components/ui/card";
 import { DottedSeparator } from "@/components/dotted-separator";
 
 import { Task } from "@/features/tasks/types";
+import { Projects } from "@/features/projects/types";
 import { useGetTasks } from "@/features/tasks/api/use-get-tasks";
 import { useGetMembers } from "@/features/members/api/use-get-members";
 import { useGetProjects } from "@/features/projects/api/use-get-projects";
@@ -18,7 +20,7 @@ import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 import { useCreateTaskModal } from "@/features/tasks/hooks/use-create-task-modal";
 import { useCreateProjectModal } from "@/features/projects/hooks/use-create-project-modal";
 import { useGetWorkspaceAnalytics } from "@/features/workspaces/api/use-get-workspace-analytics";
-import { formatDistanceToNow } from "date-fns";
+import { ProjectAvatar } from "@/features/projects/components/project-avatar";
 
 export const WorkspaceIdClient = () => {
   const workspaceId = useWorkspaceId();
@@ -36,8 +38,6 @@ export const WorkspaceIdClient = () => {
   const { data: members, isLoading: isLoadingMembers } = useGetMembers({
     workspaceId,
   });
-
-  const { open: createProject } = useCreateProjectModal();
 
   const isLoading =
     isLoadingAnalytics ||
@@ -58,6 +58,7 @@ export const WorkspaceIdClient = () => {
       <Analytics data={analytics} />
       <div className="grid grid-cols-1 xl:grid-cols-2 gap-4">
         <TaskList data={tasks.documents} total={tasks.total} />
+        <ProjectList data={projects.documents} total={projects.total} />
       </div>
     </div>
   );
@@ -111,6 +112,54 @@ export const TaskList = ({ data, total }: TaskListProps) => {
         <Button variant="muted" className="mt-4 w-full" asChild>
           <Link href={`/workspaces/${workspaceId}/tasks `}>Show All</Link>
         </Button>
+      </div>
+    </div>
+  );
+};
+
+interface ProjectListProps {
+  data: Projects[];
+  total: number;
+}
+
+export const ProjectList = ({ data, total }: ProjectListProps) => {
+  const workspaceId = useWorkspaceId();
+  const { open: createProject } = useCreateProjectModal();
+
+  return (
+    <div className="flex flex-col gap-y-4 col-span-1">
+      <div className="bg-white border rounded-lg p-4">
+        <div className="flex items-center justify-between">
+          <p className="text-lg font-semibold">Projects ({total})</p>
+          <Button onClick={createProject} variant="secondary" size="icon">
+            <PlusIcon className="size-4 text-neutral-400" />
+          </Button>
+        </div>
+        <DottedSeparator className="my-4" />
+        <ul className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+          {data.map((project) => (
+            <li key={project.$id}>
+              <Link href={`/workspaces/${workspaceId}/projects/${project.$id}`}>
+                <Card className="shadow-none rounded-lg hover:opacity-75 transition">
+                  <CardContent className="p-4 flex items-center gap-x-2.5">
+                    <ProjectAvatar
+                      name={project.name}
+                      image={project.imageUrl}
+                      className="size-12"
+                      fallbackClassName="text-lg"
+                    />
+                    <p className="text-lg font-medium truncate">
+                      {project.name}
+                    </p>
+                  </CardContent>
+                </Card>
+              </Link>
+            </li>
+          ))}
+          <li className="text-sm text-muted-foreground text-center hidden first-of-type:block">
+            No projects found
+          </li>
+        </ul>
       </div>
     </div>
   );
