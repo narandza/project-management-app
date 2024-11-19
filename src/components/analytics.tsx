4182fce17a7fb03b53bd76c00cc32a24aa7e5a2c
narandza <60319154+narandza@users.noreply.github.com>
2024-11-19 09:10:14 +0100

    create analytics card component

--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -1,11 +1,19 @@
 import { ProjectAnalyticsResponseType } from "@/features/projects/api/use-get-project-analytics";
 import { ScrollArea } from "./ui/scroll-area";
+import { AnalyticsCard } from "./analytics-card";
 
 export const Analytics = ({ data }: ProjectAnalyticsResponseType) => {
   return (
     <ScrollArea className="border rounded-lg w-full whitespace-nowrap shrink-0">
       <div className="w-full flex flex-row ">
-        <div className="flex items-center flex-1"></div>
+        <div className="flex items-center flex-1">
+          <AnalyticsCard
+            title="Total tasks"
+            value={data.taskCount}
+            variant={data.taskDifference > 1 ? "up" : "down"}
+            increaseValue={data.taskDifference}
+          />
+        </div>
       </div>
     </ScrollArea>
   );
