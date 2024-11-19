2923950f4bdb76ac8cfeb2280848424aad043ee7
narandza <60319154+narandza@users.noreply.github.com>
2024-11-19 09:40:43 +0100

    create workspace id client page

---
+++ src/app/(dashboard)/workspaces/[workspaceId]/client.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { PageError } from "@/components/page-error";
+import { PageLoader } from "@/components/page-loader";
+import { useGetMembers } from "@/features/members/api/use-get-members";
+import { useGetProjects } from "@/features/projects/api/use-get-projects";
+import { useCreateProjectModal } from "@/features/projects/hooks/use-create-project-modal";
+import { useGetTasks } from "@/features/tasks/api/use-get-tasks";
+import { useCreateTaskModal } from "@/features/tasks/hooks/use-create-task-modal";
+import { useGetWorkspaceAnalytics } from "@/features/workspaces/api/use-get-workspace-analytics";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
+
+export const WorkspaceIdClient = () => {
+  const workspaceId = useWorkspaceId();
+
+  const { data: analytics, isLoading: isLoadingAnalytics } =
+    useGetWorkspaceAnalytics({ workspaceId });
+
+  const { data: tasks, isLoading: isLoadingTasks } = useGetTasks({
+    workspaceId,
+  });
+  const { data: projects, isLoading: isLoadingProjects } = useGetProjects({
+    workspaceId,
+  });
+
+  const { data: members, isLoading: isLoadingMembers } = useGetMembers({
+    workspaceId,
+  });
+
+  const { open: createProject } = useCreateProjectModal();
+
+  const { open: createTask } = useCreateTaskModal();
+
+  const isLoading =
+    isLoadingAnalytics ||
+    isLoadingTasks ||
+    isLoadingProjects ||
+    isLoadingMembers;
+
+  if (isLoading) {
+    return <PageLoader />;
+  }
+
+  if (!analytics || !tasks || !projects || !members) {
+    return <PageError message="Failed to load workspace data" />;
+  }
+
+  return <div className="">client</div>;
+};
