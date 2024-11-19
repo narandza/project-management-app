190de10914ec1fa2e770550031f1f7042a0582a0
narandza <60319154+narandza@users.noreply.github.com>
2024-11-19 10:08:14 +0100

    create task list component

--- src/app/(dashboard)/workspaces/[workspaceId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/client.tsx
@@ -1,14 +1,24 @@
 "use client";
 
+import Link from "next/link";
+import { CalendarIcon, PlusIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Analytics } from "@/components/analytics";
 import { PageError } from "@/components/page-error";
 import { PageLoader } from "@/components/page-loader";
+import { Card, CardContent } from "@/components/ui/card";
+import { DottedSeparator } from "@/components/dotted-separator";
+
+import { Task } from "@/features/tasks/types";
+import { useGetTasks } from "@/features/tasks/api/use-get-tasks";
 import { useGetMembers } from "@/features/members/api/use-get-members";
 import { useGetProjects } from "@/features/projects/api/use-get-projects";
-import { useCreateProjectModal } from "@/features/projects/hooks/use-create-project-modal";
-import { useGetTasks } from "@/features/tasks/api/use-get-tasks";
-import { useCreateTaskModal } from "@/features/tasks/hooks/use-create-task-modal";
-import { useGetWorkspaceAnalytics } from "@/features/workspaces/api/use-get-workspace-analytics";
 import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
+import { useCreateTaskModal } from "@/features/tasks/hooks/use-create-task-modal";
+import { useCreateProjectModal } from "@/features/projects/hooks/use-create-project-modal";
+import { useGetWorkspaceAnalytics } from "@/features/workspaces/api/use-get-workspace-analytics";
+import { formatDistanceToNow } from "date-fns";
 
 export const WorkspaceIdClient = () => {
   const workspaceId = useWorkspaceId();
@@ -29,8 +39,6 @@ export const WorkspaceIdClient = () => {
 
   const { open: createProject } = useCreateProjectModal();
 
-  const { open: createTask } = useCreateTaskModal();
-
   const isLoading =
     isLoadingAnalytics ||
     isLoadingTasks ||
@@ -45,5 +53,65 @@ export const WorkspaceIdClient = () => {
     return <PageError message="Failed to load workspace data" />;
   }
 
-  return <div className="">client</div>;
+  return (
+    <div className="h-full flex flex-col space-y-4">
+      <Analytics data={analytics} />
+      <div className="grid grid-cols-1 xl:grid-cols-2 gap-4">
+        <TaskList data={tasks.documents} total={tasks.total} />
+      </div>
+    </div>
+  );
+};
+
+interface TaskListProps {
+  data: Task[];
+  total: number;
+}
+
+export const TaskList = ({ data, total }: TaskListProps) => {
+  const workspaceId = useWorkspaceId();
+  const { open: createTask } = useCreateTaskModal();
+
+  return (
+    <div className="flex flex-col gap-y-4 col-span-1">
+      <div className="bg-muted rounded-lg p-4">
+        <div className="flex items-center justify-between">
+          <p className="text-lg font-semibold">Tasks ({total})</p>
+          <Button onClick={createTask} variant="muted" size="icon">
+            <PlusIcon className="size-4 text-neutral-400" />
+          </Button>
+        </div>
+        <DottedSeparator className="my-4" />
+        <ul className="flex flex-col gap-y-4">
+          {data.map((task) => (
+            <li key={task.$id}>
+              <Link href={`/workspaces/${workspaceId}/tasks/${task.$id}`}>
+                <Card className="shadow-none rounded-lg hover:opacity-75 transition">
+                  <CardContent className="p-4">
+                    <p className="text-lg font-medium truncate">{task.name}</p>
+                    <div className="flex items-center gap-x-2">
+                      <p className="">{task.project?.name}</p>
+                      <div className="size-1 rounded-full bg-neutral-300" />
+                      <div className="text-sm text-muted-foreground flex items-center">
+                        <CalendarIcon className="size-3 mr-1" />
+                        <span className="truncate">
+                          {formatDistanceToNow(new Date(task.dueDate))}
+                        </span>
+                      </div>
+                    </div>
+                  </CardContent>
+                </Card>
+              </Link>
+            </li>
+          ))}
+          <li className="text-sm text-muted-foreground text-center hidden first-of-type:block">
+            No tasks found
+          </li>
+        </ul>
+        <Button variant="muted" className="mt-4 w-full" asChild>
+          <Link href={`/workspaces/${workspaceId}/tasks `}>Show All</Link>
+        </Button>
+      </div>
+    </div>
+  );
 };
