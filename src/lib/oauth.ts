79be9f0c5a2dd3c3e61ed6f58639d6172514e8de
narandza <60319154+narandza@users.noreply.github.com>
2024-11-20 10:52:09 +0100

    fixing oauth

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -1,6 +1,5 @@
 "use server";
 
-import { headers } from "next/headers";
 import { createAdminClient } from "./appwrtie";
 import { redirect } from "next/navigation";
 import { OAuthProvider } from "node-appwrite";
@@ -8,14 +7,10 @@ import { OAuthProvider } from "node-appwrite";
 export async function signUpWithGithub() {
   const { account } = await createAdminClient();
 
-  const origin = headers().get("origin");
-
-  console.log(origin);
-
   const redirectUrl = await account.createOAuth2Token(
     OAuthProvider.Github,
-    `${origin}/oauth`,
-    `${origin}/sign-up`
+    `${process.env.NEXT_PUBLIC_APP}/oauth`,
+    `${process.env.NEXT_PUBLIC_APP}/sign-up`
   );
 
   return redirect(redirectUrl);
@@ -24,14 +19,10 @@ export async function signUpWithGithub() {
 export async function signUpWithGoogle() {
   const { account } = await createAdminClient();
 
-  const origin = headers().get("origin");
-
-  console.log(origin);
-
   const redirectUrl = await account.createOAuth2Token(
     OAuthProvider.Google,
-    `${origin}/oauth`,
-    `${origin}/sign-up`
+    `${process.env.NEXT_PUBLIC_APP}/oauth`,
+    `${process.env.NEXT_PUBLIC_APP}/sign-up`
   );
 
   return redirect(redirectUrl);
