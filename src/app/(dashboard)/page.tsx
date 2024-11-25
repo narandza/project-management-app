8ab5884495eae79ccbc8bd32afd64eb7dbc67071
narandza <60319154+narandza@users.noreply.github.com>
2024-11-25 08:40:18 +0100

    minor fix

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,12 +1,12 @@
-import { getCurrent } from "@/features/auth/queries";
+// import { getCurrent } from "@/features/auth/queries";
 import { getWorkspaces } from "@/features/workspaces/queries";
 
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const user = await getCurrent();
+  // const user = await getCurrent();
 
-  if (!user) redirect("/sign-in");
+  // if (!user) redirect("/sign-in");
 
   const workspaces = await getWorkspaces();
 
