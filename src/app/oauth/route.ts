8d2635d936248888e5d133df3e5f4915d3b127a3
Nikola Mladenović <nikolanik999@gmail.com>
2024-12-03 13:22:14 +0100

    fix: same site lax

--- src/app/oauth/route.ts
+++ src/app/oauth/route.ts
@@ -1,27 +1,27 @@
-import { AUTH_COOKIE } from "@/features/auth/constants";
-import { createAdminClient } from "@/lib/appwrtie";
-import { cookies } from "next/headers";
-import { NextRequest, NextResponse } from "next/server";
+import { AUTH_COOKIE } from "@/features/auth/constants"
+import { createAdminClient } from "@/lib/appwrtie"
+import { cookies } from "next/headers"
+import { NextRequest, NextResponse } from "next/server"
 
 export async function GET(request: NextRequest) {
-  const userId = request.nextUrl.searchParams.get("userId");
+  const userId = request.nextUrl.searchParams.get("userId")
 
-  const secret = request.nextUrl.searchParams.get("secret");
+  const secret = request.nextUrl.searchParams.get("secret")
 
-  const { account } = await createAdminClient();
+  const { account } = await createAdminClient()
 
   if (!userId || !secret) {
-    throw new NextResponse("Missing fields", { status: 400 });
+    throw new NextResponse("Missing fields", { status: 400 })
   }
 
-  const session = await account.createSession(userId, secret);
+  const session = await account.createSession(userId, secret)
 
   cookies().set(AUTH_COOKIE, session.secret, {
     path: "/",
     httpOnly: true,
-    sameSite: "strict",
-    secure: true,
-  });
+    sameSite: "lax",
+    secure: true
+  })
 
-  return NextResponse.redirect(`${request.nextUrl.origin}/`);
+  return NextResponse.redirect(`${request.nextUrl.origin}/`)
 }
